5dea80c5d056fa1d4cbeb359a13b29758943a941
John McIntosh <john.mcintosh@dosh.cash>
2020-02-29 15:33:10 -0600

    Add a Core library that Feature librbary depends on, but linking occurs in the host app.

--- MyApp/Core/Core.swift
+++ MyApp/Core/Core.swift
@@ -1,19 +1,16 @@
 //
-//  Feature.swift
-//  Feature
+//  Core.swift
+//  Core
 //
 //  Created by John McIntosh on 2/29/20.
 //  Copyright © 2020 Dosh. All rights reserved.
 //
 
-import Core
-
-public class Feature {
-
+public class Core {
+    
     public init() { }
     
     public func doStuff() {
-        print("Hello from feature")
-        Core().doStuff()
+        print("Hello from core")
     }
 }
