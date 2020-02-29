77af3767fb025ce115023ac3b3227f53d8d61374
John McIntosh <john.mcintosh@dosh.cash>
2020-02-29 15:19:39 -0600

    Create basic app

---
+++ MyApp/MyApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by John McIntosh on 2/29/20.
+//  Copyright © 2020 Dosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Hello from app")
+    }
+}
